d7871fc58c9cd17b65b07622ea3283c797fdc464
DDimitr0va <desi.dim@yahoo.com>
2021-02-03 09:33:37 +0200

    Code update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,14 @@
+
 public class Main {
 
     public static void main(String[] args) {
-        String myString1 = "My name is Test";
-        System.out.println(myString1);
-        myString1 = myString1.replace("Test", "Desislava");
-        System.out.println(myString1);
+        String myString1 = "My name is %s";
+        System.out.println(myString1.replace("My", "Your"));
         String[] words = myString1.split(" ");
         System.out.println(words.length);
-        String myString2 = "My name is Desislava";
-        System.out.println(myString1.equalsIgnoreCase(myString2));
-        String myString3 = ".";
-        System.out.println(myString1 + myString3);
-
+        System.out.println(myString1.equals("My name is "));
+        System.out.println(String.format(myString1, "Desislava"));
     }
 }
+
+
